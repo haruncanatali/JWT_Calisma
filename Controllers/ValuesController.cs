7af48eb051cb6cf82cbd7b5d7623a9d753bf974c
Haruncan Atalı <77530565+haruncanatali@users.noreply.github.com>
2021-10-04 17:25:34 +0300

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,7 +1,9 @@
 using JWT_Calisma.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +16,115 @@ namespace JWT_Calisma.Controllers
     public class ValuesController : ControllerBase
     {
         AppDbContext context;
-        public ValuesController(AppDbContext context)
+        UserManager<Kullanici> userManager;
+
+        public ValuesController(AppDbContext context,UserManager<Kullanici> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Authorize]
+        [Route("removeCard")]
+        public async Task<IActionResult> RemoveBasket(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user!=null)
+            {
+                Sepet card = context.Tbl_Card.Where(c => c.UserId == userId).Include(c => c.ProductCard).First();
+                context.Tbl_ProductCard.RemoveRange(card.ProductCard);
+                context.SaveChanges();
+                return Ok(new string[]
+                {
+                    "The deletion was successfull!"
+                });
+            }
+            else
+            {
+                return BadRequest(new string[]
+                {
+                    "User not found!"
+                });
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        [Route("getUserCard")]
+        public async Task<IActionResult> GetUserCard(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user!=null)
+            {
+                Sepet card = context.Tbl_Card.Where(c => c.UserId == userId).Include(c=>c.ProductCard).First();
+                if (card.ProductCard.Count()>0)
+                {
+                    List<Product> productList = new List<Product>();
+                    foreach (var item in card.ProductCard)
+                    {
+                        productList.Add(context.Tbl_Product.FirstOrDefault(c => c.Id == item.ProductId));
+                    }
+                    return Ok(productList);
+                }
+                else
+                {
+                    return BadRequest(new string[] 
+                    { 
+                        "Cart is empty!"
+                    });
+                }
+            }
+            else
+            {
+                return BadRequest(new string[]
+                {
+                    "User not found!"
+                });
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        [Route("addToBasket")]
+        public async Task<IActionResult> AddToBasket([FromBody]ProductCardDto productList)
+        {
+            var user = await userManager.FindByIdAsync(productList.UserId);
+            var card = context.Tbl_Card.Where(c=>c.UserId == user.Id).First();
+            if (user != null)
+            {
+                try
+                {
+                    foreach (var item in productList.ProductCard)
+                    {
+                        context.Tbl_ProductCard.Add(new ProductCard
+                        {
+                            CardId = card.Id,
+                            ProductId = item.ProductId
+                        });
+                        context.SaveChanges();
+                    }
+
+                    return Ok(new string[]
+                    {
+                        "Adding was successfull!"
+                    });
+                }
+                catch (Exception exception)
+                {
+                    return BadRequest(new string[]
+                    {
+                        "Error :"+exception.ToString()
+                    });
+                }
+            }
+            else
+            {
+                return BadRequest(new string[]
+                {
+                    "User not found!"
+                });
+            }
         }
 
         [Authorize]
